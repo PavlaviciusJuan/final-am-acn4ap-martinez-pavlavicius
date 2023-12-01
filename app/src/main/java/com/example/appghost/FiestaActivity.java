b0077edb79f8b41104e21e5174f8f555aa723662
PavlaviciusJuan <juan.pavlavicius@davinci.edu.ar>
2023-12-01 18:36:24 -0300

    commit: boton que agrega texto

--- app/src/main/java/com/example/appghost/FiestaActivity.java
+++ app/src/main/java/com/example/appghost/FiestaActivity.java
@@ -5,13 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FiestaActivity extends AppCompatActivity {
-
+    TextView texto;
+    ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiesta);
+
+        texto = (TextView)findViewById(R.id.textView3);
+        texto.setText("Ghost Party es una fiesta de generos variados!");
+        imagen = (ImageView)findViewById(R.id.imageView);
+        Button boton = findViewById(R.id.cambio_descripcion);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout scroll = (LinearLayout) findViewById(R.id.linear_layout);
+                    LinearLayout linearLayout = new LinearLayout(boton.getContext());
+                        TextView myText = new TextView(boton.getContext());
+                        myText.setText("Ven a Ghost Party!");
+                        linearLayout.setOrientation(LinearLayout.VERTICAL);
+                    linearLayout.addView(myText);
+                scroll.addView(linearLayout);
+            }
+        });
+
     }
 
     public void onClick(View view) {
@@ -21,6 +44,8 @@ public class FiestaActivity extends AppCompatActivity {
             startActivity(miIntent);
         } else if (view.getId() == R.id.btn_login) {
 
+        } else if (view.getId() == R.id.cambio_imagen) {
+            imagen.setImageResource(R.drawable.predeterminada2);
         }
     }
 }
--- app/src/main/java/com/example/appghost/MainActivity.java
+++ app/src/main/java/com/example/appghost/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onClick(View view) {
@@ -22,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
         } else if (view.getId() == R.id.btn_login) {
 
         }
-
-
 
 
     }
